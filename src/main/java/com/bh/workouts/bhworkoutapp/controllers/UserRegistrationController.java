c4e695c51073eff53493d6278474a4f24d0d1c88
Andrii Maliuta <quadr988@gmail.com>
2019-03-24 18:10:26 +0200

    create own exercises

--- src/main/java/com/bh/workouts/bhworkoutapp/controllers/UserRegistrationController.java
+++ src/main/java/com/bh/workouts/bhworkoutapp/controllers/UserRegistrationController.java
@@ -2,11 +2,8 @@ package com.bh.workouts.bhworkoutapp.controllers;
 
 import com.bh.workouts.bhworkoutapp.models.User;
 import com.bh.workouts.bhworkoutapp.services.ExerciseNameService;
-import com.bh.workouts.bhworkoutapp.services.InitExercisesService;
 import com.bh.workouts.bhworkoutapp.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
