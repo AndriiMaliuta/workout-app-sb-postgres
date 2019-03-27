08abfdfe59c4f3448c1f7881a512313b93ddd1a7
anma0513 <quadr988@gmail.com>
2019-03-27 17:36:11 +0200

    overall exercise items

--- src/main/java/com/bh/workouts/bhworkoutapp/controllers/workout/OtherMonthWorkoutController.java
+++ src/main/java/com/bh/workouts/bhworkoutapp/controllers/workout/OtherMonthWorkoutController.java
@@ -69,7 +69,29 @@ public class OtherMonthWorkoutController {
 
         switch (monthName) {
             case "JANUARY" : monthInt = 1;
+                break;
             case "FEBRUARY" : monthInt = 2;
+                break;
+            case "MARCH" : monthInt = 3;
+                break;
+            case "APRIL" : monthInt = 4;
+                break;
+            case "MAY" : monthInt = 5;
+                break;
+            case "JUNE" : monthInt = 6;
+                break;
+            case "JULY" : monthInt = 7;
+                break;
+            case "AUGUST" : monthInt = 8;
+                break;
+            case "SEPTEMBER" : monthInt = 9;
+                break;
+            case "OCTOBER" : monthInt = 10;
+                break;
+            case "NOVEMBER" : monthInt = 11;
+                break;
+            case "DECEMBER" : monthInt = 12;
+                break;
         }
 
         YearMonth yearMonth = YearMonth.of(monthForm.getYear(), monthInt);
