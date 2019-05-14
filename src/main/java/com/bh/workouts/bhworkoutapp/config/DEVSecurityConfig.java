25fcf109512c894d6f3ea5714ad835f400ee8a37
anma0513 <quadr988@gmail.com>
2019-05-14 14:08:16 +0300

    - exercise list is created inly when exercises do not exist

--- src/main/java/com/bh/workouts/bhworkoutapp/config/DEVSecurityConfig.java
+++ src/main/java/com/bh/workouts/bhworkoutapp/config/DEVSecurityConfig.java
@@ -1,19 +1,19 @@
-package com.bh.workouts.bhworkoutapp.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-@EnableWebSecurity
-@Profile("DEV")
-public class DEVSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity security) throws Exception {
-
-        security.httpBasic().disable();
-    }
-}
+//package com.bh.workouts.bhworkoutapp.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Profile;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Profile("DEV")
+//@Configuration
+//@EnableWebSecurity
+//public class DEVSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(HttpSecurity security) throws Exception {
+//
+//        security.httpBasic().disable();
+//    }
+//}
