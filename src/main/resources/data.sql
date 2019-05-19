681e8f1cb85bf1986ab5852da59203b18d0470d5
Andrii Maliuta <quadr988@gmail.com>
2019-05-19 16:56:25 +0300

    for heroku

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-# insert into exercise_name values (4, 'PECS', 'asdasd', 1);
-insert into users (user_id, login, password,first_name, last_name, active, email)
-values (550, 'login_1', 'pass_1' , 'Vasyl', 'Vasylenko', 1, 'some1@mail.com');
+--# insert into exercise_name values (4, 'PECS', 'asdasd', 1);
+--insert into users (user_id, login, password,first_name, last_name, active, email)
+--values (550, 'login_1', 'pass_1' , 'Vasyl', 'Vasylenko', 1, 'some1@mail.com');
