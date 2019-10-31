290875e94c96a166c797fdb56a2a3459b60cc475
anma0513 <quadr988@gmail.com>
2019-10-31 12:16:55 +0200

    navbar changes

--- src/main/java/com/bh/workouts/bhworkoutapp/config/ProPTest.java
+++ src/main/java/com/bh/workouts/bhworkoutapp/config/ProPTest.java
@@ -1,4 +1,4 @@
-//package com.bh.workouts.bhworkoutapp.config;
+/package com.bh.workouts.bhworkoutapp.config;
 //
 //import lombok.Data;
 //import org.springframework.boot.context.properties.ConfigurationProperties;
