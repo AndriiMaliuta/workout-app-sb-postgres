48ded053131a6b49e31856e9bf8b9751ae24d6c5
anma0513 <quadr988@gmail.com>
2019-11-01 12:38:00 +0200

    Bulma

--- src/main/java/com/bh/workouts/bhworkoutapp/controllers/StatisticsController.java
+++ src/main/java/com/bh/workouts/bhworkoutapp/controllers/StatisticsController.java
@@ -1,11 +1,27 @@
 package com.bh.workouts.bhworkoutapp.controllers;
 
+import com.bh.workouts.bhworkoutapp.services.StatisticsService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 
 @Controller
 public class StatisticsController {
 
-    
+    private final StatisticsService statisticsService;
+
+    @Autowired
+    public StatisticsController(StatisticsService statisticsService) {
+        this.statisticsService = statisticsService;
+    }
+
+    public String getStats(Model model) {
+
+        // statisticsService.getMonthWorkouts(dateFrom, dateTo)
+
+        return "/workout/stats";
+    }
 
 
 }
