0488a155293100cb8d04c546f46f35bc37b439fe
anma0513 <quadr988@gmail.com>
2019-11-01 12:26:11 +0200

    Bulma

--- src/main/java/com/bh/workouts/bhworkoutapp/repositories/ExerciseRepository.java
+++ src/main/java/com/bh/workouts/bhworkoutapp/repositories/ExerciseRepository.java
@@ -2,11 +2,9 @@ package com.bh.workouts.bhworkoutapp.repositories;
 
 import com.bh.workouts.bhworkoutapp.models.Exercise;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Controller;
 
-@Controller
 public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
 
-    Exercise findByTitle(String title);
+    
 
 }
