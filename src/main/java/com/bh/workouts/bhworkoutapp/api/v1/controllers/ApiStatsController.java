7c27b426ea85bb83e271f875d293a8b2258109f4
Andrii Maliuta <quadr988@gmail.com>
2020-05-06 15:44:50 +0300

    1.3.2 - fixing statistics for year

---
+++ src/main/java/com/bh/workouts/bhworkoutapp/api/v1/controllers/ApiStatsController.java
@@ -0,0 +1,15 @@
+//package com.bh.workouts.bhworkoutapp.api.v1.controllers;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping(ApiStatsController.REST_API_URL)
+//public class ApiStatsController {
+//
+//    static final String REST_API_URL = "/rest/api/v1/stats/workouts/year-month";
+//
+//    @GetMapping
+//    public
+//}
