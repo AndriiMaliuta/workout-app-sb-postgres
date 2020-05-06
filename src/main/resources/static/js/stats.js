481b186c2799e8344f0e7554dd5bd9d58736edee
Andrii Maliuta <quadr988@gmail.com>
2020-05-06 15:20:47 +0300

    1.3.2 - fixing statistics for year

--- src/main/resources/static/js/stats.js
+++ src/main/resources/static/js/stats.js
@@ -1,1 +1,8 @@
 console.log('Working!!!')
+
+const yearSelect = document.querySelector('#year-select');
+const yearBtn = document.querySelector('year-confirm-btn');
+
+yearSelect.addEventListener('change', e => {
+    console.log(yearSelect.value)
+});
